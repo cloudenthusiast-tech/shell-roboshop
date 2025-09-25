0436207b5b21b03e8ec4020f156a77f4b27b683b
Aravind <kolanuaravindreddy@gmail.com>
2025-09-25 10:50:27 +0530

    automate-ec2

--- instance.sh
+++ instance.sh
@@ -2,7 +2,7 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0097d14ad3a0baf5f"
 
-for instance in "$@"
+for instance in $@
 do 
 INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
    
