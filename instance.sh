e2b8bb0bfd69b293a521381e8e64fbb905f63a17
Aravind <kolanuaravindreddy@gmail.com>
2025-09-25 11:45:47 +0530

    automate-ec2

--- instance.sh
+++ instance.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0097d14ad3a0baf5f"
+HOSTED_ZONE_ID="Z10446792H6QP6XMMVXFH"
+DOMAIN_NAME="kolanu.space"
 
 for instance in $@
 do 
@@ -9,10 +11,30 @@ do
    
    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"   #catalogue.kolanu.space
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME"    #kolanu.space
      fi
 
 echo "$instance: $IP"
 
-done
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $HOSTED_ZONE_ID\
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
+  done
