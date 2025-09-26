f13680615115d0f37622e8d98b84ef49a8acc0d9
Aravind <kolanuaravindreddy@gmail.com>
2025-09-26 15:43:24 +0530

    some commit

--- instance.sh
+++ instance.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0097d14ad3a0baf5f" # replace with your SG ID
-ZONE_ID="Z10446792H6QP6XMMVXFH" # replace with your ID
+SG_ID="sg-0097d14ad3a0baf5f"
+ZONE_ID="Z10446792H6QP6XMMVXFH" 
 DOMAIN_NAME="kolanu.space"
-
 for instance in $@ # mongodb redis mysql
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
@@ -15,7 +14,7 @@ do
         RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.kolanu.space
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
+        RECORD_NAME="$DOMAIN_NAME" # kolanu.space
     fi
 
     echo "$instance: $IP"
