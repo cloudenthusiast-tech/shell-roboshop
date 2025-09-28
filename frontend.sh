0eb8e54c7fa25372cd0c157c6e6b9e7608041af0
Aravind <kolanuaravindreddy@gmail.com>
2025-09-28 12:33:39 +0530

    some commit

--- frontend.sh
+++ frontend.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[37m"
-LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 
-START_TIME=$(date +%s)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-   echo -e " $R error:: please run with root user previliges $N"
-   exit 1  # will exit from the script execution if encountered error
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
-  if [ $1 -ne 0 ]; then
-   echo -e "$2 ... $R FAILURE $N" 
-   exit 1
-  else
-   echo -e "$2 ..... $G  SUCCESS $N"  
-fi
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
-dnf module disable nginx -y  &>>$LOG_FILE
-dnf module enable nginx:1.24 -y  &>>$LOG_FILE
-dnf install nginx -y   &>>$LOG_FILE
+dnf module disable nginx -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx 
+systemctl enable nginx  &>>$LOG_FILE
 systemctl start nginx 
+VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/*   &>>$LOG_FILE
-
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip  &>>$LOG_FILE
-
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip  &>>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend"
 
-cp $SCRIPT_DIR/nginx.conf  /etc/nginx/nginx.conf
-systemctl daemon-reload
+rm -rf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
+
 systemctl restart nginx 
-
-END_TIME=$(date +%s)
-TOTAL_SCRIPT_TIME=$(($END_TIME-$START_TIME))
-echo -e "script executed in:$G $TOTAL_SCRIPT_TIME seconds"
+VALIDATE $? "Restarting Nginx"
